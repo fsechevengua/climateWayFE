a055df154d9c62dd179a533610433d287dfa6a25
Filipe <filipe.echevengua@softerize.com>
2018-01-30 22:13:42 -0200

    números heatmap

--- js/thermal-amplitude.js
+++ js/thermal-amplitude.js
@@ -33,22 +33,8 @@ $(document).ready(function () {
         });
     });
 
-    function getThermalAmplitude() {
-        var weatherDataCall = $.ajax({
-            url: "http://localhost:9000/thermalAmplitude",
-            type: "GET",
-            data: {
-                date: month,
-            }
-        });
-
-        var weatherDataPromise = Promise.resolve(weatherDataCall).then(function (data) {
-
-        });
-    }
-
     // Monta o Heatmap
-    function generateHeatmap(id, config, isMonths) {
+    async function generateHeatmap(id, config, isMonths) {
         var margin = {
             top: 50,
             right: 0,
@@ -99,58 +85,29 @@ $(document).ready(function () {
             });
 
         var timeLabels = svg.selectAll(".timeLabel")
-            .data(config.months)
-            .enter().append("text")
-            .text(function (d) {
-                return d;
-            })
-            .attr("x", function (d, i) {
-                return i * gridSize;
-            })
-            .attr("y", 0)
-            .style("text-anchor", "middle")
-            .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-            .attr("class", function (d, i) {
-                return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis");
-            });
-
-        var data = [{
-            day: 1,
-            week: 1,
-            value: 100
-        }, {
-            day: 1,
-            week: 2,
-            value: 100
-        }, {
-            day: 1,
-            week: 3,
-            value: 100
-        }, {
-            day: 1,
-            week: 4,
-            value: 100
-        }, {
-            day: 1,
-            week: 5,
-            value: 100
-        }, {
-            day: 1,
-            week: 6,
-            value: 100
-        }, {
-            day: 1,
-            week: 7,
-            value: 100
-        }, {
-            day: 3,
-            week: 45,
-            value: 100
-        }, {
-            day: 7,
-            week: 1,
-            value: 100
-        }];
+        .data(config.months)
+        .enter().append("text")
+        .text(function (d) {
+            return d;
+        })
+        .attr("x", function (d, i) {
+            return i * gridSize;
+        })
+        .attr("y", 0)
+        .style("text-anchor", "middle")
+        .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+        .attr("class", function (d, i) {
+            return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis");
+        });
+        
+        let heatmapFetch = $.ajax({
+            url: "http://localhost:9000/heatmap",
+            type: "GET",
+        });
+    
+        let data = await Promise.resolve(heatmapFetch).then(function (data) {
+            return data;
+        });
 
         var heatmapChart = function (tsvFile) {
             var colorScale = d3.scale.quantile()
@@ -163,6 +120,11 @@ $(document).ready(function () {
                 .data(data, function (d) {
                     return d.day + ':' + d.week;
                 });
+
+            var texts = svg.selectAll(".week")
+            .data(data, function (d) {
+                return d.day + ':' + d.week;
+            });
 
             cards.append("title");
 
@@ -182,6 +144,19 @@ $(document).ready(function () {
                 .attr("width", gridSize)
                 .attr("height", gridSize)
                 .style("fill", colors[0]);
+
+            texts.enter().append('text')
+                .text( function (d) { 
+                    return d.date;
+                })
+                .attr("class", "week bordered day-cell")
+                .attr("x", function (d) {
+                    return (d.week) * gridSize - 20;
+                })
+                .attr("y", function (d) {
+                    return (d.day) * gridSize -7;
+                })
+                .attr('fill', 'white');
 
             cards.transition().duration(1000)
                 .style("fill", function (d) {
@@ -248,4 +223,24 @@ $(document).ready(function () {
         $(".back-heatmonth").remove();
         generateHeatmap("#heat-map-months", configYear, false);
     });
+
+    function getWeekNumber(d) {
+        // Copy date so don't modify original
+        d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+        // Set to nearest Thursday: current date + 4 - current day number
+        // Make Sunday's day number 7
+        d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+        // Get first day of year
+        var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+        // Calculate full weeks to nearest Thursday
+        var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+        // Return array of year and week number
+        return weekNo;
+    }
+
+    let heatmapData = [];
+    
+    function getHeatmapData(type){
+        
+    }
 });
